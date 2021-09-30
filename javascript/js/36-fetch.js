1b3ac91c28664be4ffc306fe5ba945a7580fb01b
cesardileo18 <cesardileo18@gmail.com>
2021-09-30 16:32:21 -0300

    cambios

--- javascript/js/36-fetch.js
+++ javascript/js/36-fetch.js
@@ -1,6 +1,12 @@
 'use strict'
 
 //Fetch peticiones a servicios / apis rest
-
+var usuarios = [];
 fetch('https://jsonplaceholder.typicode.com/users')
+//Con el then recogo los datos, el callback es data, recibe data y lo convierte a json
       .then(data => data.json())
+      //Recogemos de nuvo la data, funcion de callback
+      .then(users =>{
+        usuarios = users.data;
+        console.log('Usuarios---------------',usuarios)
+      })
