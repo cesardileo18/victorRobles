9bef950034c4d0e853971f3c94caa07693ac3c7d
cesardileo18 <cesardileo18@gmail.com>
2021-10-01 18:52:59 -0300

    cambios

--- javascript/js/36-fetch.js
+++ javascript/js/36-fetch.js
@@ -2,23 +2,27 @@
 
 var div_usuarios = document.querySelector("#usuarios")
 var div_janet = document.querySelector("#janet")
+var div_profesor = document.querySelector("#profesor")
 
 
       getUsuarios()
-//Con el then recogo los datos, el callback es data, recibe data y lo convierte a json
+      //Con el then recogo los datos, el callback es data, recibe data y lo convierte a json
       .then(data => data.json())
       //Fetch peticiones a servicios / apis rest
       //Recogemos de nuvo la data, funcion de callback
       .then(users =>{
         listadoUsuarios(users);
+        return getInfo();
+      })
+      .then(user =>{
+        console.log('profesor',user)
+        div_profesor.innerHTML = user;
         return getJanet();
       })
       .then(data=>data.json())
       .then(user =>{
-
-        mostrarJanet(user.data)
+        mostrarJanet(user.data);
       })
-      ;
 
       function getUsuarios(){
         return fetch('https://jsonplaceholder.typicode.com/users')
@@ -26,7 +30,25 @@ var div_janet = document.querySelector("#janet")
       function getJanet(){
         return fetch('https://reqres.in/api/users/2')
       }
-      
+      function getInfo(){
+        var profesor = {
+            nombre: 'Victor',
+            apellidos: 'Di Leonardo',
+            url: 'https://dileoweb.com'
+        };
+
+        return new Promise((resolve, reject)=>{
+        var profesor_string = "";
+          setTimeout(function(){
+                    //Convertir info en json string
+            profesor_string = JSON.stringify(profesor);
+
+            // if(typeof profesor_string != 'string'|| profesor_string != '')return reject('error1');
+            return resolve(profesor_string);
+           },3000);
+          });
+      }
+
       function listadoUsuarios(usuarios){
           usuarios.map((user, i) =>{
 
@@ -46,16 +68,12 @@ var div_janet = document.querySelector("#janet")
           let avatar = document.createElement('img')
 
           nombre.innerHTML = user.first_name ;
-          console.log('Imagen----------------------------------',user.avatar)
           avatar.src = user.avatar;
           user.avatar = avatar;
           avatar.width="100";
           div_janet.appendChild(nombre);
-          console.log(`entra1`)
           div_janet.appendChild(avatar);
-          console.log(avatar)
       
-        
           function mensaje() {
             document.querySelector(".loading2").style.display = 'none';
           }
