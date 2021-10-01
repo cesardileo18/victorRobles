e442ef61b9d80e7846cf78ba0b27f1c73f509bcf
cesardileo18 <cesardileo18@gmail.com>
2021-10-01 11:21:04 -0300

    cambios

--- javascript/js/36-fetch.js
+++ javascript/js/36-fetch.js
@@ -2,23 +2,25 @@
 
 var div_usuarios = document.querySelector("#usuarios")
 //Fetch peticiones a servicios / apis rest
-var usuarios = [];
+
 fetch('https://jsonplaceholder.typicode.com/users')
 //Con el then recogo los datos, el callback es data, recibe data y lo convierte a json
       .then(data => data.json())
       //Recogemos de nuvo la data, funcion de callback
       .then(users =>{
-        usuarios = users;
-        usuarios.map((user, i) =>{
+        listadoUsuarios(users)
+      });
+      function listadoUsuarios(usuarios){
+          usuarios.map((user, i) =>{
 
             let nombre = document.createElement('h4')
             nombre.innerHTML = i +". "+ user.name + " - " +user.username;
             div_usuarios.appendChild(nombre)
-         
+        
           
             function mensaje() {
               document.querySelector(".loading").style.display = 'none';
-             }
-             setTimeout(mensaje,1000);
+            }
+            setTimeout(mensaje,1000);
         })
-      })
+      }
