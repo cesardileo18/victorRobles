1b21e75c2f95eab80c7763ea1baff9c0cfadeadf
cesardileo18 <cesardileo18@gmail.com>
2021-10-29 10:34:17 -0300

    jQuery

---
+++ jquery/js/01-selectores.js
@@ -0,0 +1,13 @@
+//Cuando el documento esta listo, nos referimos al document.ready
+$(document).ready(function(){
+
+    //Selector de id
+$("#rojo").css("background", "red")
+          .css("color", "white");
+
+$("#amarillo").css("background", "yellow")
+              .css("color", "black");
+
+$("#verde").css("background", "green")
+           .css("color", "black");
+})
